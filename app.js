c673dd56b9e1ce4e844e051c594295df53353ea2
shidqi <nauvalholic@gmail.com>
2020-12-15 13:28:54 +0700

    Initial commit

--- app.js
+++ app.js
@@ -1,64 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 
+// serve auto-generated swagger-ui generated API docs from express
+const swaggerUi = require('swagger-ui-express');
+
+// YAML parser
+const YAML = require('yamljs');
+
+// To parse .yaml file into json format
+const swaggerDocument = YAML.load('./swagger.yaml');
 const port = process.env.PORT || 5000;
 
-// Extended: https://swagger.io/specification/#infoObject
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      version: "1.0.0",
-      title: "Customer API",
-      description: "Customer API Information",
-      contact: {
-        name: "Amazing Developer"
-      },
-      servers: ["http://localhost:5000"]
-    }
-  },
-  // ['.routes/*.js']
-  apis: ["app.js"]
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// Routes
-/**
- * @swagger
- * /customers:
- *  get:
- *    description: Use to request all customers
- *    responses:
- *      '200':
- *        description: A successful response
- */
-app.get("/customers", (req, res) => {
-  res.status(200).send("Customer results");
-});
-
-/**
- * @swagger
- * /customers:
- *    put:
- *      description: Use to return all customers
- *    parameters:
- *      - name: customer
- *        in: query
- *        description: Name of our customer
- *        required: false
- *        schema:
- *          type: string
- *          format: string
- *    responses:
- *      '201':
- *        description: Successfully created user
- */
-app.put("/customer", (req, res) => {
-  res.status(200).send("Successfully updated customer");
-});
+// Full documentation: https://github.com/scottie1984/swagger-ui-express
+app.use('/', swaggerUi.serve, swaggerUi.setup(JSON.parse(JSON.stringify(swaggerDocument))));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
